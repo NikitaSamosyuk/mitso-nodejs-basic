cf7bf18ce45da1050a5ccc9e7413f3a8e97432c3
student <fels.fes@gmail.com>
2026-02-16 12:32:43 +0300

    Complete fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
 const create = async () => {
-    // Write your code here 
+    fs.writeFile('files/fresh.txt', 'I am fresh and young', { flag: 'wx'}, (err) => {
+        if(err){
+            return console.log('FS operation failed');
+        }
+    });
 };
 
 await create();

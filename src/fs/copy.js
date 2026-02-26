edf1d9c798a1ecab56c421b2cecac9df6c32b3a0
student <student@mitso.by>
2026-02-26 10:23:08 +0300

    Complete fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
-const copy = async () => {
-    // Write your code here 
+import fs from 'fs';
+const copy = async() => {
+    fs.access('files', (err) => {
+        if (err) {
+            return console.log("You need folder /files");
+        }
+        fs.mkdir('files_copy', (err) => {
+            if (err) {
+                return console.log('Create folder failed');
+            }
+            fs.cp('files', 'files_copy', { recursive: true }, (err) => {
+                if (err) {
+                    return console.log('Copy files failed');
+                }
+                return console.log('Operation complete');
+            });
+        });
+    });
 };
-
 await copy();

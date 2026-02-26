f0553fa0704b5d30de045ab27a9bcd00c59904a3
student <student@mitso.by>
2026-02-26 10:37:04 +0300

    Complete fs/rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
-const rename = async () => {
-    // Write your code here 
+import fs from 'fs';
+const rename = async() => {
+    const oldFile = 'files/wrongFilename.txt';
+    const newFile = 'files/wrongFilename.md';
+
+    fs.access(newFile, (err) => {
+        if (!err) {
+            return console.log('Failed. You have a file md');
+        }
+        fs.access(oldFile, (err) => {
+            if (err) {
+                return console.log("Failed. You haven't a file txt");
+            }
+            fs.rename(oldFile, newFile, (err) => {
+                if (err) {
+                    return console.log('FS operation failed');
+                }
+                console.log("Operation complete");
+            })
+        })
+    })
 };
 
 await rename();

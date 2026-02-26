8e1b1331584cde88647ef8ab5abc314150804cdb
student <student@mitso.by>
2026-02-26 10:50:39 +0300

    Complete fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
-const remove = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+const remove = async() => {
 
+    const needFile = 'files/fileToRemove.txt';
+
+    fs.access(needFile, (err) => {
+        if (err) {
+            return console.log('Failed. You need a file');
+        }
+        fs.rm(needFile, (err) => {
+            if (err) {
+                return console.log('FS operation failed');
+            }
+            console.log('Operation Complete');
+        });
+    });
+};
 await remove();

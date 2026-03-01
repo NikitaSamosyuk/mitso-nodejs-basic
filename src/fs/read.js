2795ce342d0ffc29137c70bc55d65f8891bc68b1
Никита <scorpionzero530@gmail.com>
2026-03-01 17:16:42 +0300

    Complete fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
 const read = async () => {
-    // Write your code here 
-};
+    try {
+            const Files = await fs.promises.readFile('./files/fileToRead.txt', 'utf8');
+            console.log(Files);
+        }
+           catch (err) {
+            console.log('FS operation Failed');
+           } 
+        }; 
 
 await read();

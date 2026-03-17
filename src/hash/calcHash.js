51460d54621001b09b4ef6028d2ec5d11b05b820
Никита <scorpionzero530@gmail.com>
2026-03-17 20:37:38 +0300

    Complete hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const input = createReadStream('./files/fileToCalculateHashFor.txt');
+
+    input.on('data', chunk => hash.update(chunk));
+    
+    input.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+
+    input.on('error', (err) => {
+        console.error('Файл не найден или ошибка чтения');
+    });
 };
 
 await calculateHash();

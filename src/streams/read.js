76a446466798b42e242704f10f30f6dfc7795bfb
Никита <scorpionzero530@gmail.com>
2026-03-17 21:14:53 +0300

    Complete streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs'
+
 const read = async () => {
-    // Write your code here 
+    // Создаем URL относительно текущего файла
+    // './files/fileToRead.txt' — путь от скрипта к файлу
+    const filePath = new URL('./files/fileToRead.txt', import.meta.url);
+
+    const stream = createReadStream(filePath, 'utf8');
+
+    stream.pipe(process.stdout);
+
+    return new Promise((resolve, reject) => {
+        stream.on('end', resolve);
+        stream.on('error', reject);
+    });
 };
 
 await read();
